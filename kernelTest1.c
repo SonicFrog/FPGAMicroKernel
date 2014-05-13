dd8a06a8557f355040b25cffc83de5838ac8e075
tOverney <tristan.overney@epfl.ch>
2014-05-14 00:53:24 +0200

    Some logging added

--- kernelTest1.c
+++ kernelTest1.c
@@ -220,10 +220,6 @@ int main() {
 	dummyMonitor1 = createMonitor();
 	dummyMonitor2 = createMonitor();
 
-        displayNumber(0, 9);
-
-        sleep(1);
-
 	createProcess(consumer0, STACK_SIZE);
 	createProcess(consumer1, STACK_SIZE);
 	createProcess(consumer2, STACK_SIZE);
